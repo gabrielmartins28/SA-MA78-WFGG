0e41f6467252d92bfd736dbee87c185845e103b6
gabrielmartins28 <gabriel_martins28@estudante.sesisenai.org.br>
2026-07-09 20:09:51 -0300

    Remove unused imports and function call

--- estrutura_inicial_python/main.py
+++ estrutura_inicial_python/main.py
@@ -1,7 +1,1 @@
-from viagem import listar_viagem
-from viagem import atualizar_viagem
-from viagem import deletar_viagem
-from viagem import inserir_viagem
 
-
-atualizar_viagem(300, "TESTE", "2023-10-01", "2023-10-05")
