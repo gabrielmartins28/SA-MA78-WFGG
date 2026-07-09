b842969df54ffd58d47712db0fe9a75b171fcc5e
GABRIEL BAPTISTA MARTINS <gabriel_martins28@estudante.sesisenai.org.br>
2026-07-09 20:08:51 -0300

    Atualizando a main

---
+++ estrutura_inicial_python/main.py
@@ -0,0 +1,7 @@
+from viagem import listar_viagem
+from viagem import atualizar_viagem
+from viagem import deletar_viagem
+from viagem import inserir_viagem
+
+
+atualizar_viagem(300, "TESTE", "2023-10-01", "2023-10-05")
